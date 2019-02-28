619f937ce0e0344bbc7221e25e703a9b57bcc80a
michael <michael.medvedskiy.private@gmail.con>
2019-02-28 20:26:32 +0300

    changed all /work/... paths to /content/drive/My drive/work/...

--- training/config_training.py
+++ training/config_training.py
@@ -1,9 +1,9 @@
-config = {'stage1_data_path':'/work/DataBowl3/stage1/stage1/',
-          'luna_raw':'/work/DataBowl3/luna/raw/',
-          'luna_segment':'/work/DataBowl3/luna/seg-lungs-LUNA16/',
+config = {'stage1_data_path':'/content/drive/My drive/work/DataBowl3/stage1/stage1/',
+          'luna_raw':'/content/drive/My drive/work/DataBowl3/luna/raw/',
+          'luna_segment':'/content/drive/My drive/work/DataBowl3/luna/seg-lungs-LUNA16/',
           
-          'luna_data':'/work/DataBowl3/luna/allset',
-          'preprocess_result_path':'/work/DataBowl3/stage1/preprocess/',       
+          'luna_data':'/content/drive/My drive/work/DataBowl3/luna/allset',
+          'preprocess_result_path':'/content/drive/My drive/work/DataBowl3/stage1/preprocess/',
           
           'luna_abbr':'./detector/labels/shorter.csv',
           'luna_label':'./detector/labels/lunaqualified.csv',
